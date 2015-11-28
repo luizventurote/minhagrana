6b5941317b9f64d4363a4006ddc2524c11311f14
luizventurote <luiz_17@ymail.com>
2015-11-28 11:07:32 -0200

    Novo projeto adicionado

---
+++ app/src/main/java/luizventurote/minhagrana/MainActivity.java
@@ -0,0 +1,176 @@
+package luizventurote.minhagrana;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Save header or result
+     */
+    private AccountHeader headerResult = null;
+    private Drawer result = null;
+    private boolean opened = false;
+
+    /**
+     * Floating action button
+     */
+    private FloatingActionsMenu menuMultipleActions;
+
+    /**
+     * ListView
+     */
+    private ListView mainListView ;
+    private ArrayAdapter<String> listAdapter ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Show Drawer
+        this.showDrawer(savedInstanceState);
+
+        // Show Float Action Button
+        this.showFloatActionButton();
+
+        // Show ListView
+        this.showListView();
+    }
+
+    /**
+     * Show drawer with toolbar
+     *
+     * @param savedInstanceState
+     */
+    private void showDrawer(Bundle savedInstanceState) {
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Create the drawer
+        result = new DrawerBuilder(this)
+                .withRootView(R.id.drawer_container)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggleAnimated(true)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_home).withIcon(FontAwesome.Icon.faw_money).withIdentifier(1),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_settings).withIcon(FontAwesome.Icon.faw_cog).withIdentifier(2)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+
+                        if (drawerItem != null) {
+
+                            Intent intent = null;
+
+                            if (drawerItem.getIdentifier() == 1) {
+                                Log.d("LOG", "Click item 1");
+                            } else if (drawerItem.getIdentifier() == 2) {
+                                Log.d("LOG", "Click item 2");
+                            }
+
+                        }
+
+                        return false;
+                    }
+
+                })
+                .withSavedInstance(savedInstanceState)
+                .build();
+    }
+
+    /**
+     * Show Float Action Button
+     */
+    private void showFloatActionButton() {
+
+        this.menuMultipleActions = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
+
+        findViewById(R.id.action_a).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked pink Floating Action Button", Toast.LENGTH_SHORT).show();
+                ShowDialogAddValue();
+            }
+        });
+
+        findViewById(R.id.action_b).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked pink Floating Action Button", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     * Show List View
+     */
+    private void showListView() {
+
+        // Find the ListView resource.
+        mainListView = (ListView) findViewById( R.id.listViewValues );
+
+        // Create and populate a List of planet names.
+        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
+                "Jupiter", "Saturn", "Uranus", "Neptune"};
+        ArrayList<String> planetList = new ArrayList<String>();
+        planetList.addAll( Arrays.asList(planets) );
+
+        // Create ArrayAdapter using the planet list.
+        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, planetList);
+
+        // Add more planets. If you passed a String[] instead of a List<String>
+        // into the ArrayAdapter constructor, you must not add more items.
+        // Otherwise an exception will occur.
+        listAdapter.add( "Ceres" );
+        listAdapter.add( "Pluto" );
+        listAdapter.add( "Haumea" );
+        listAdapter.add( "Makemake" );
+        listAdapter.add( "Eris" );
+
+        // Set the ArrayAdapter as the ListView's adapter.
+        mainListView.setAdapter( listAdapter );
+    }
+
+    /**
+     * Open Dialog to Add a value
+     */
+    private void ShowDialogAddValue() {
+
+        new MaterialDialog.Builder(this)
+                .title(R.string.dialog_input_text_add_title)
+                .content(R.string.dialog_input_text_add)
+                .inputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD)
+                .input(R.string.dialog_input_text_add_hint, R.string.dialog_input_text_add_prefill, new MaterialDialog.InputCallback() {
+                    @Override
+                    public void onInput(MaterialDialog dialog, CharSequence input) {
+                        // Do something
+                    }
+                }).show();
+    }
+}
