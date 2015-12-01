2e79264b7875993177dfc5cb0f999643f573842a
paulo <paulohenrique.de93@hotmail.com>
2015-11-30 23:20:54 -0200

    adicionar gasto na lista implementado

--- app/src/main/java/luizventurote/minhagrana/MainActivity.java
+++ app/src/main/java/luizventurote/minhagrana/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.InputType;
@@ -136,6 +137,7 @@ public class MainActivity extends AppCompatActivity {
                 // Toast.makeText(MainActivity.this, "Clicked pink Floating Action Button", Toast.LENGTH_SHORT).show();
                 // ShowDialogAddValue();
                 startActivity(new Intent(context, MovimentacaoFinanceiraActivity.class));
+                finish();
             }
         });
 
@@ -148,22 +150,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Show List View
+     * Exibe a ListView
      */
     private void showListView() {
 
-        // Find the ListView resource.
-        mainListView = (ListView) findViewById( R.id.listViewValues );
-
-        // Create and populate a List of planet names.
         String[] de = {"descricao", "valor"};
         int[] para = {R.id.descricao, R.id.valor};
         SimpleAdapter adapter = new SimpleAdapter(this, listarGastos(), R.layout.itens_da_lista_gasto, de, para);
 
+        //Resgatando recurso ListView do XML
         ListView lv = (ListView) findViewById(R.id.listViewValues);
+
         lv.setAdapter(adapter);
         lv.setOnItemClickListener(chamaAtividades(this));
-
     }
 
     public AdapterView.OnItemClickListener chamaAtividades(final Context context){
@@ -200,10 +199,13 @@ public class MainActivity extends AppCompatActivity {
             mov = lista_gastos.get(j);
 
             item.put("descricao", mov.getDescricao());
-            item.put("valor", mov.getValor());
+            item.put("valor", mov.getValor().toString());
             gastos.add(item);
+            item = new HashMap<String, Object>();
 
             j++;
+
+
         }
 
         return gastos;
